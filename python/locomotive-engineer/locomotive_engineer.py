ef3a976eddb4a4e578d8f1c91d479b0a7432d81c
Hama <pashazad@msu.edu>
2024-12-24 17:24:47 -0500

    Implement get_list_of_wagons function to return a list of wagons from arbitrary arguments

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -1,13 +1,15 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons():
+def get_list_of_wagons(*args):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
+    wagons = list(args)
+
+    return wagons
 
 
 def fix_list_of_wagons(each_wagons_id, missing_wagons):
